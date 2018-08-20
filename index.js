fc2613b987758949c742017f4c27e4848c80c9fe
Timothy Kaing <tk.rain09@gmail.com>
2018-08-19 23:01:05 -0700

    Added Giphy Search App

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
